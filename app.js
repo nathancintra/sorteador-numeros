bbff061990df2272d6fbdcc7a9c1c9deae6fb2a1
Nathan Cintra <nathancintra@gmai.com>
2025-02-10 22:09:03 -0300

    Implementação fa função Math.Randon

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 function sortear(){
-    let quantidade = document.getElementById('quantidade').value;
-    let de = document.getElementById('de').value;
-    let ate = document.getElementById('ate').value;
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
 
-    alert(`Quantidade ${quantidade}` );
-    alert(`De ${de}`);
-    alert(`Até o número ${ate}`);
+    let sorteados = [];
+    let numero;
+
+    for (let i = 0; i < quantidade; i++){
+        numero = numeroAleatorio(de, ate);
+        sorteados.push(numero);
+    }
+    
+    alert(sorteados);
+
+}
+
+function numeroAleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
