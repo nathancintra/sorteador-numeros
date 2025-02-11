f29d517353c31025df83bbce777e565078a31e75
Nathan Cintra <nathancintra@gmai.com>
2025-02-10 21:31:16 -0300

    #

---
+++ app.js
@@ -0,0 +1,9 @@
+function sortear(){
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+
+    alert(`Quantidade ${quantidade}` );
+    alert(`De ${de}`);
+    alert(`Até o número ${ate}`);
+}
