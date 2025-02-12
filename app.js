aeae5a3eb74299d9bd47999b4ba1b831047d7c8b
Nathan Cintra <nathancintra@gmai.com>
2025-02-11 21:45:05 -0300

    Atualização:
    Habilitando o botão de reiniciar.
    Zerando o resultado.

--- app.js
+++ app.js
@@ -12,15 +12,34 @@ function sortear(){
         while (sorteados.includes(numero)){
             numero = numeroAleatorio(de, ate);
         }
-        
+
         sorteados.push(numero);
     }
 
     let resultado = document.getElementById('resultado');
     resultado.innerHTML= `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`
-
+    alterarBotaoReiniciar();
 }
 
 function numeroAleatorio(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarBotaoReiniciar(){
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar(){
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = `<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>`;
+    alterarBotaoReiniciar();
 }
